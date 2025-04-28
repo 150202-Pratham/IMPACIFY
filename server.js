4d81a6e9e53ae131a03269a1527fa62c7d5e9d14
Satyam Godara <89057441+Satyam-Godara@users.noreply.github.com>
2025-04-28 21:41:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use('/auth', authRoutes);
 app.use('/orders', orderRoutes);
 app.use('/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
